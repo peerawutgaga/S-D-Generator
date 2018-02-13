83b9a56077480b16154e57041fe22d017aa94ae3
peerawutgaga <pee.luang45@gmail.com>
2018-02-13 16:04:35 +0700

    Make it uploadable.

--- Script/Main.js
+++ Script/Main.js
@@ -1,26 +1,38 @@
-// Get the modal
 var modal = document.getElementById("uploadModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("uploadBtn");
-
-// Get the <span> element that closes the modal
+var uploadBtn = document.getElementById("uploadBtn");
 var span = document.getElementsByClassName("close")[0];
+var uploadSDBtn =document.getElementById("uploadSD");
+var SDuploader = document.getElementById("SDuploader");
+var uploadCDBtn =document.getElementById("uploadCD");
+var CDuploader = document.getElementById("CDuploader");
 
-// When the user clicks the button, open the modal 
-btn.style.cursor = "pointer";
-btn.onclick = function() {
-    modal.style.display = "block";
+uploadBtn.style.cursor = "pointer";
+uploadBtn.onclick = function () {
+	modal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+span.onclick = function () {
+	modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
 };
+
+uploadSDBtn.onclick = function(){
+	SDuploader.click();
+};
+uploadCDBtn.onclick = function(){
+	CDuploader.click();
+};
+
+function uploadSDFile()
+{
+	
+}
+function uploadCDFile()
+{
+	
+}
