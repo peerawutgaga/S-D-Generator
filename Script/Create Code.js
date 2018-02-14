d20bc49667ebd39b80583c9e8610084222ec2174
peerawutgaga <pee.luang45@gmail.com>
2018-02-14 14:29:37 +0700

    add element to create code page (unfinished)

---
+++ Script/Create Code.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+function addItemToSelect()
+{
+var select = document.getElementById("SDSelect");
+var option = document.createElement('option');
+option.text = option.value = 30;
+select.add(option, 0);
+    
+}
