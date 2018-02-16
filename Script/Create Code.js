9784c38899062637e57f81a9757aeda3397ccf7c
peerawutgaga <pee.luang45@gmail.com>
2018-02-16 14:45:23 +0700

    make editor balance bracket

--- Script/Create Code.js
+++ Script/Create Code.js
@@ -1,10 +1,47 @@
-document.getElementById("codeEditor").onkeydown = function(e) {
+var isShiftDown = false;
+var codeEditor = document.getElementById("codeEditor");
+codeEditor.onkeydown = function(e) {
+    console.log(e.keyCode);
     if (e.keyCode === 9) {
-    var val = this.value,
+        var val = this.value,
         start = this.selectionStart,
         end = this.selectionEnd;
-    this.value = val.substring(0, start) + '\t' + val.substring(end);
-    this.selectionStart = this.selectionEnd = start + 1;
-    return false;
+        this.value = val.substring(0, start) + '\t' + val.substring(end);
+        this.selectionStart = this.selectionEnd = start + 1;
+        return false;
     }
+    else if(e.keyCode === 16){
+        isShiftDown = true;
+        return false;
+    }
+    else if(e.keyCode === 57 && isShiftDown){
+        var val = this.value,
+        start = this.selectionStart,
+        end = this.selectionEnd;
+        this.value = val.substring(0, start) + '()' + val.substring(end);
+        this.selectionStart = this.selectionEnd = start + 1;
+        return false;
+    }
+    else if(e.keyCode === 219 && isShiftDown){
+        var val = this.value,
+        start = this.selectionStart,
+        end = this.selectionEnd;
+        this.value = val.substring(0, start) + '{}' + val.substring(end);
+        this.selectionStart = this.selectionEnd = start + 1;
+        return false;
+    }
+    else if(e.keyCode === 219 && !isShiftDown){
+        var val = this.value,
+        start = this.selectionStart,
+        end = this.selectionEnd;
+        this.value = val.substring(0, start) + '[]' + val.substring(end);
+        this.selectionStart = this.selectionEnd = start + 1;
+        return false;
+    }
+    
+};
+codeEditor.onkeyup = function(e){
+    if(e.keyCode === 16){
+        isShiftDown = false;
+    } 
 };
