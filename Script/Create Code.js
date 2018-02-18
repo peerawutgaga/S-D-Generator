d82a410657203e506dab07cac669e932b2f9edb2
peerawutgaga <pee.luang45@gmail.com>
2018-02-18 08:35:23 +0700

    Add image, css and js draft

--- Script/Create Code.js
+++ Script/Create Code.js
@@ -1,7 +1,6 @@
 var isShiftDown = false;
 var codeEditor = document.getElementById("codeEditor");
 codeEditor.onkeydown = function(e) {
-    console.log(e.keyCode);
     if (e.keyCode === 9) {
         var val = this.value,
         start = this.selectionStart,
@@ -38,7 +37,22 @@ codeEditor.onkeydown = function(e) {
         this.selectionStart = this.selectionEnd = start + 1;
         return false;
     }
-    
+    else if(e.keyCode === 222 && isShiftDown){
+        var val = this.value,
+        start = this.selectionStart,
+        end = this.selectionEnd;
+        this.value = val.substring(0, start) + '\"\"' + val.substring(end);
+        this.selectionStart = this.selectionEnd = start + 1;
+        return false;
+    }
+    else if(e.keyCode === 222 && !isShiftDown){
+        var val = this.value,
+        start = this.selectionStart,
+        end = this.selectionEnd;
+        this.value = val.substring(0, start) + '\'\'' + val.substring(end);
+        this.selectionStart = this.selectionEnd = start + 1;
+        return false;
+    }
 };
 codeEditor.onkeyup = function(e){
     if(e.keyCode === 16){
