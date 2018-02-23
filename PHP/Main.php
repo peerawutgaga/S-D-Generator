10aa47071e3fa0e044e115d494025c9a11af7179
peerawutgaga <pee.luang45@gmail.com>
2018-02-23 10:41:17 +0700

    Make page not redirect after uploaded.

--- PHP/Main.php
+++ PHP/Main.php
@@ -1,32 +1,35 @@
 <?php
     if(isset($_POST['SDSubmit']))
     {
-        upload("../Class Diagrams/", "SDFile");
+        upload("../Sequence Diagrams/", "SDFile");
     }
     else if(isset($_POST['CDSubmit'])){
-        upload("../Sequence Diagrams/", "CDFile");
+        upload("../Class Diagrams/", "CDFile");
     }
     function upload($target_dir, $diagramType)
     {
         $target_file = $target_dir . basename($_FILES[$diagramType]["name"]);
         $uploadOk = 1;
         $xmlFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        if (file_exists($target_file)) {
+            alert("File already exist.");
+            return;
+        }
 
-        // Check file size
-        if($xmlFileType != "xml") {
-            echo "Sorry, only XML files are allowed.";
-            $uploadOk = 0;
+        if($xmlFileType != "xml" ) {
+            alert("Only XML file is allowed");
+            return;
         }
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.";
-        // if everything is ok, try to upload file
+        if (move_uploaded_file($_FILES[$diagramType]["tmp_name"], $target_file)) {
+            alert("The file ". basename( $_FILES[$diagramType]["name"]). " has been uploaded.");
         } else {
-            if (move_uploaded_file($_FILES[$diagramType]["tmp_name"], $target_file)) {
-                echo "The file ". basename( $_FILES[$diagramType]["name"]). " has been uploaded.";
-            } else {
-                echo "Sorry, there was an error uploading your file.";
-            }
+            alert("Upload failed.");
         }
+    }
+    function alert($message){
+        echo "<script type='text/javascript'>
+            alert('$message');
+            window.location.href='../index.html';
+        </script>";
     }
 ?> 
