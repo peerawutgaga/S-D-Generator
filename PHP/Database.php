abdadf7afa88d3a87051e57e12ba2acf1fee8d51
peerawutgaga <pee.luang45@gmail.com>
2018-02-26 19:53:16 +0700

    Add create source code table SQL

--- PHP/Database.php
+++ PHP/Database.php
@@ -17,7 +17,7 @@
         }
         return $db_selected;
     }
-    function createIfNotExist($db_name,$conn){
+    function createIfNotExist($conn, $db_name){
         $sql = "CREATE DATABASE IF NOT EXISTS $db_name";
         if ($conn->query($sql) === TRUE) {
             echo "Database created successfully";
@@ -26,40 +26,72 @@
         }
     }
     function createCallGraphTable($conn){
-        $createGraphTable =  "CREATE TABLE IF NOT EXISTS graphTable(
+        $createGraphTableSQL =  "CREATE TABLE IF NOT EXISTS graphTable(
             graphID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
             graphName VARCHAR(30) NOT NULL,
             createDate TIMESTAMP
         )";
-        $createNodeTable =  "CREATE TABLE IF NOT EXISTS nodeTable(
+        $createNodeTableSQL =  "CREATE TABLE IF NOT EXISTS nodeTable(
             nodeID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
             nodeName VARCHAR(30) NOT NULL,
             graphID INT(6) NOT NULL
         )";
-        $createMessageTable =  "CREATE TABLE IF NOT EXISTS messageTable(
+        $createMessageTableSQL =  "CREATE TABLE IF NOT EXISTS messageTable(
             messageID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
             messageName VARCHAR(30) NOT NULL,
             sentNodeID INT(6) NOT NULL,
             receivedNodeID INT(6) NOT NULL
         )";
-        if ($conn->query($createGraphTable) === TRUE) {
+        if ($conn->query($createGraphTableSQL) === TRUE) {
             echo "Graph table created successfully";
         } else {
             echo "Error creating graph table: " . $conn->error;
         }
-        if ($conn->query($createNodeTable) === TRUE) {
+        if ($conn->query($createNodeTableSQL) === TRUE) {
             echo "Node table created successfully";
         } else {
             echo "Error creating node table: " . $conn->error;
         }
-        if ($conn->query($createMessageTable) === TRUE) {
+        if ($conn->query($createMessageTableSQL) === TRUE) {
             echo "Message table created successfully";
         } else {
             echo "Error creating message table: " . $conn->error;
         }
-        function initialDiagramFileDB($conn){
-
+    }
+    function createClassDiagramTable($conn){
+        $createFileTableSQL = "CREATE TABLE IF NOT EXISTS fileTable(
+            fileID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            fileName VARCHAR(30) NOT NULL,
+            fileLocation VARCHAR(100) NOT NULL,
+            createDate TIMESTAMP
+        )";
+        if ($conn->query($createFileTableSQL) === TRUE) {
+            echo "Class diagram table created successfully";
+        } else {
+            echo "Error creating class diagram table: " . $conn->error;
         }
+    }
+    function createSourceCodeTable($conn){
+        $createFileTableSQL = "CREATE TABLE IF NOT EXISTS fileTable(
+            fileID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            fileName VARCHAR(30) NOT NULL,
+            fileType VARCHAR(4) NOT NULL,
+            fileLocation VARCHAR(100) NOT NULL,
+            createDate TIMESTAMP
+        )";
+        if ($conn->query($createFileTableSQL) === TRUE) {
+            echo "Source code table created successfully";
+        } else {
+            echo "Error creating source code table: " . $conn->error;
+        }
+    }
+    function testFunc(){
+        $conn = connectToDB();
+        createIfNotExist($conn,'ClassDiagram');
+        $success = selectDB($conn,'ClassDiagram');
+        echo "$success";
+        createClassDiagramTable($conn);
+        $conn->close();
     }
 
 
--- PHP/Main.php
+++ PHP/Main.php
@@ -1,7 +1,6 @@
 <?php
     require "Database.php";
-    if(isset($_POST['SDSubmit']))
-    {
+    if(isset($_POST['SDSubmit'])){
         upload("../Sequence Diagrams/", "SDFile");
     }
     else if(isset($_POST['CDSubmit'])){
