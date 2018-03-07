c01dc9bf20d4815d2b8f2be4ede20f97f5d486f8
peerawutgaga <pee.luang45@gmail.com>
2018-03-07 17:31:49 +0700

    implement class diagram processor (unfinished).

--- PHP/CDProcessor.php
+++ PHP/CDProcessor.php
@@ -2,6 +2,7 @@
     require_once "ClassDiagramService.php";
     class CDProcessor{
         private static $conn;
+        private static $diagramID;
         public static function readClassDiagram($filename,$targetFile){
             $xml = simplexml_load_file($targetFile);
             if ($xml === false) {
@@ -11,16 +12,24 @@
                 }
                 die("XMLProcessor Terminated. Please open console to see errors");
             }
+            self::saveFileToDB($fileName,$targetFile);
+            if($xml['Xml_structure'] == 'simple'){
+                self::processSimpleCD($xml);
+            }else{
+                self::processTraditionalCD($xml);
+            }
         }
-        private static function saveFileToDB($fileName,$targetFile){
+        private static function saveFileToDB($fileName,$fileTarget){
             self::$conn = Database::connectToDB();
             //Database::dropDatabase(self::$conn,'classDiagram');
+            ClassDiagramService::initialClassDiagramDatabase(self::$conn, $fileName, $fileTarget);
+            self::$diagramID = ClassDiagramService::selectFromDiagramTable('diagramID','diagramName',$fileName);
+        }
+        private static function processSimpleCD($xml){
+            $classList = $xml->Models->Package->ModelChildren->Package->ModelChildren;
             
         }
-        private static function processSimpleCD(){
-
-        }
-        private static function processTraditionalCD(){
+        private static function processTraditionalCD($xml){
             
         }
     }
--- PHP/ClassDiagramService.php
+++ PHP/ClassDiagramService.php
@@ -5,7 +5,7 @@
             $sql =  "CREATE TABLE IF NOT EXISTS diagram(
                 diagramID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
                 diagramName VARCHAR(30) NOT NULL,
-                fileName VARCHAR(100) NOT NULL,
+                fileTarget VARCHAR(100) NOT NULL,
                 createDate TIMESTAMP
             )";
             if ($conn->query($sql) === FALSE) {
@@ -15,9 +15,9 @@
         private static function createClassTable($conn){
             $sql = "CREATE TABLE IF NOT EXISTS class(
                 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+                diagramID INT(6) NOT NULL,
                 classID VARCHAR(16) NOT NULL,
-                className VARCHAR(30) NOT NULL,
-                diagramID INT(6) NOT NULL
+                className VARCHAR(30) NOT NULL
             )";
             if ($conn->query($sql) === FALSE) {
                 echo "Error at creating graph table: ".$conn->error."<br>";
@@ -25,11 +25,12 @@
         }
         private static function createMethodTable($conn){
             $sql = "CREATE TABLE IF NOT EXISTS method(
-                methodID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                diagramID INT(6) NOT NULL,
+                classID VARCHAR(16) NOT NULL,
+                methodID VARCHAR(16) NOT NULL, 
                 methodName VARCHAR(50) NOT NULL,
-                parameterCount INT NOT NULL,
-                returnType VARCHAR(30) NOT NULL,
-                classID INT(6) NOT NULL
+                returnType VARCHAR(30)
             )";
             if ($conn->query($sql) === FALSE) {
                 echo "Error at creating graph table: ".$conn->error."<br>";
@@ -37,10 +38,12 @@
         }
         private static function createParameterTable($conn){
             $sql = "CREATE TABLE IF NOT EXISTS parameter(
-                parameterID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                diagramID INT(6) NOT NULL,
+                methodID VARCHAR(16) NOT NULL,
+                parameterID VARCHAR(16) NOT NULL, 
                 parameterName VARCHAR(30) NOT NULL,
-                parameterType VARCHAR(30) NOT NULL,
-                methodID INT(6) NOT NULL
+                parameterType VARCHAR(30) NOT NULL
             )";
             if ($conn->query($sql) === FALSE) {
                 echo "Error at creating graph table: ".$conn->error."<br>";
@@ -53,6 +56,51 @@
             self::createClassTable($conn);
             self::createMethodTable($conn);
             self::createParameterTable($conn);
+        }
+
+        public static function insertToDiagramTable($conn, $diagramName, $fileTarget){
+            $sql = $conn->prepare("INSERT INTO diagram(diagramName, fileTarget) VALUES(?,?)");
+            $sql->bind_param("ss",$diagramName,$fileTarget);
+            if($sql->execute()===FALSE){
+                    echo "Error at inserting to diagram table: ".$sql->error."<br>";
+            }
+            $sql->close();
+        }
+        public static function insertToClassTable($conn, $diagramID, $classID, $className){
+            $sql = $conn->prepare("INSERT INTO class(diagramID, classID, className) VALUES(?,?,?)");
+            $sql->bind_param("iss",$diagramID,$classID, $className);
+            if($sql->execute()===FALSE){
+                    echo "Error at inserting to class table: ".$sql->error."<br>";
+            }
+            $sql->close();
+        }
+        public static function insertToMethodTable($conn, $diagramID, $classID, $methodID, $methodName, $returnType){
+            $sql = $conn->prepare("INSERT INTO method(diagramID, classID, methodID, methodName, returnType) VALUES(?,?,?,?,?)");
+            $sql->bind_param("issss",$diagramID,$classID, $methodID, $methodName, $returnType);
+            if($sql->execute()===FALSE){
+                    echo "Error at inserting to method table: ".$sql->error."<br>";
+            }
+            $sql->close();
+        }
+        public static function insertToParameterTable($conn, $diagramID, $methodID, $parameterID, $parameterName, $parameterType){
+            $sql = $conn->prepare("INSERT INTO parameter(diagramID,methodID, parameterID, parameterName, parameterType) VALUES(?,?,?,?,?)");
+            $sql->bind_param("issss",$diagramID,$methodID, $parameterID, $parameterName, $parameterType);
+            if($sql->execute()===FALSE){
+                    echo "Error at inserting to parameter table: ".$sql->error."<br>";
+            }
+            $sql->close();
+        }
+        public static function selectFromDiagramTable($value,$field,$keyword){
+            $conn = Database::connectToDBUsingPDO('classdiagram');
+            if($field == 'diagramID'){
+                $sql = $conn->prepare("SELECT * FROM graph WHERE diagramID = :keyword LIMIT 1");
+            }else if($field == 'diagramName'){
+                $sql = $conn->prepare("SELECT * FROM graph WHERE diagramName = :keyword LIMIT 1");
+            }
+            $sql->bindParam(':keyword',$keyword);
+            $sql->execute();
+            $result = $sql->fetch();
+            return $result[$value];
         }
     }
 ?>
